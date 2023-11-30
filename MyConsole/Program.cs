b07fddf68af4dd2ede4604427d7b3cdd4d4f4796
yakappo <93400062+yakappo@users.noreply.github.com>
2023-11-30 08:42:42 +0300

    Изменение из gogs

--- MyConsole/Program.cs
+++ MyConsole/Program.cs
@@ -11,7 +11,9 @@ namespace MyConsole
         static void Main(string[] args)
         {
             Console.WriteLine("Привет, Мир!");
-
+            Console.WriteLine("Работа с git, используя Visual Studio 2019");
+            Console.WriteLine("Эта строка добавлена из Gogs");
+            Console.WriteLine("И эта строка тоже добавлена из Gogs");
         }
     }
 }
